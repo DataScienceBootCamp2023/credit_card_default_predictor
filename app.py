d3d5693d11a327b15837603b0966a2e66f3e39d2
DataScienceMLGrpM <121807393+DataScienceMLGrpM@users.noreply.github.com>
2023-01-12 21:49:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from wsgiref import simple_server
 from flask import Flask, request,render_template, jsonify
 from flask import Response
 from flask_cors import CORS
-from flask_cors import cross_origin
+#from flask_cors import cross_origin
 from logistic_deploy import predObj
 import numpy as np
 
@@ -30,72 +30,55 @@ def predictRoute():
         if request.is_json:
             data = request.json
             try:
-			#if request.json['data'] is not None:
-				limit_bal = float(data['Limit_bal'])
-				male = float(data['male'])
-				female = float(data['female'])
-				grad_school = float(data['grad_school'])
-				university = float(data['university'])
-				high_school = float(data['high_school'])
-				edu_others = float(data['edu_others'])
-				married = float(data['married'])
-				single = float(data['single'])
-				marriage_others = float(data['marriage_others'])
-				age = float(data['age'])
-				pay_1 = float(data['pay_1'])
-				pay_2 = float(data['pay_2'])
-				pay_3 = float(data['pay_3'])
-				pay_4 = float(data['pay_4'])
-				pay_5 = float(data['pay_5'])
-				pay_6 = float(data['pay_6'])
-				bill_amt_1 = float(data['bill_amt_1'])
-				bill_amt_2 = float(data['bill_amt_2'])
-				bill_amt_3 = float(data['bill_amt_3'])
-				bill_amt_4 = float(data['bill_amt_4'])
-				bill_amt_5 = float(data['bill_amt_5'])
-				bill_amt_6 = float(data['bill_amt_6'])
-				pay_amt_1 = float(data['pay_amt_1'])
-				pay_amt_2 = float(data['pay_amt_2'])
-				pay_amt_3 = float(data['pay_amt_3'])
-				pay_amt_4 = float(data['pay_amt_4'])
-				pay_amt_5 = float(data['pay_amt_5'])
+                limit_bal = float(data['Limit_bal'])
+                male = float(data['male'])
+                female = float(data['female'])
+                grad_school = float(data['grad_school'])
+                university = float(data['university'])
+                high_school = float(data['high_school'])
+                edu_others = float(data['edu_others'])
+                married = float(data['married'])
+                single = float(data['single'])
+                marriage_others = float(data['marriage_others'])
+                age = float(data['age'])
+                pay_1 = float(data['pay_1'])
+                pay_2 = float(data['pay_2'])
+                pay_3 = float(data['pay_3'])
+                pay_4 = float(data['pay_4'])
+                pay_5 = float(data['pay_5'])
+                pay_6 = float(data['pay_6'])
+                bill_amt_1 = float(data['bill_amt_1'])
+                bill_amt_2 = float(data['bill_amt_2'])
+                bill_amt_3 = float(data['bill_amt_3'])
+                bill_amt_4 = float(data['bill_amt_4'])
+                bill_amt_5 = float(data['bill_amt_5'])
+                bill_amt_6 = float(data['bill_amt_6'])
+                pay_amt_1 = float(data['pay_amt_1'])
+                pay_amt_2 = float(data['pay_amt_2'])
+                pay_amt_3 = float(data['pay_amt_3'])
+                pay_amt_4 = float(data['pay_amt_4'])
+                pay_amt_5 = float(data['pay_amt_5'])
 				pay_amt_6 = float(data['pay_amt_6'])
+				
+				# Prepare the input data in the format required by the model
+				input_data = [limit_bal, male, female, grad_school, university, high_school, edu_others, married, single, marriage_others, age, pay_1, pay_2, pay_3, pay_4,pay_5, pay_6, bill_amt_1, bill_amt_2, bill_amt_3, bill_amt_4, bill_amt_5, bill_amt_6, pay_amt_1, pay_amt_2, pay_amt_3, pay_amt_4, pay_amt_5, pay_amt_6]
+		# Make the prediction using the model
+		prediction = self.predObj.predict(input_data)
+		
+		# Prepare the response to be returned to the client
+		response = jsonify({'prediction': prediction})
+		
+		# Return the response
+		return response
+	except ValueError:
+		return jsonify({'error': 'Invalid input data. Please check your input and try again.'})
+	except Exception as e:
+		return jsonify({'error': str(e)})
 
-				print('Here 5')
+#Create an instance of the ClientApi class
+api = ClientApi()
 
-				# data = request.json['data']
-				data = {'limit_bal':limit_bal,'male':male,'female':female,'grad_school':grad_school,'university':university,'high_school':high_school,'edu_others':edu_others,
-					'married':married,'single':single,'marriage_others':marriage_others,'age':age,
-					'pay_1':pay_1,'pay_2':pay_2,'pay_3':pay_3,'pay_4':pay_4,'pay_5':pay_5,
-					'pay_6':pay_6,'bill_amt_1':bill_amt_1,
-					'bill_amt_2':bill_amt_2,'bill_amt_3':bill_amt_3,'bill_amt_4':bill_amt_4,'bill_amt_5':bill_amt_5,'bill_amt_6':bill_amt_6,
-					'pay_amt_1':pay_amt_1,'pay_amt_2':pay_amt_2,
-					'pay_amt_3':pay_amt_3,'pay_amt_4':pay_amt_4,'pay_amt_5':pay_amt_5,'pay_amt_6':pay_amt_6}
-					
-				print('data is:     ', data)
-				pred=predObj()
-				res = pred.predict_log(data)
-
-				#result = clntApp.predObj.predict_log(data)
-				print('result is        ',res)
-				return render_template('results.html',result=res)
-			except ValueError:
-				return Response("Value not found")
-			except Exception as e:
-				print('exception is   ',e)
-				return Response(e)
-		 
-if __name__ == "__main__":
-    print('Here 1')
-    
-    clntApp = ClientApi()
-    print('Here 2')
-    host = '0.0.0.0'
-    port = 5000
-    app.run(debug=True)
-    httpd = simple_server.make_server(host, port, app)
-    print("Serving on %s %d" % (host, port))
-    httpd.serve_forever()
-	
-	
-  
+#Run the application
+if name == 'main': 
+	port = int(os.environ.get("PORT", 5000)) 
+	app.run(host='0.0.0.0', port=port)
