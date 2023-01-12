2a0db85b782bd8469a98fb9bff0fc98b74c9079f
DataScienceMLGrpM <121807393+DataScienceMLGrpM@users.noreply.github.com>
2023-01-12 22:02:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from wsgiref import simple_server
 from flask import Flask, request,render_template, jsonify
 from flask import Response
 from flask_cors import CORS
-#from flask_cors import cross_origin
+from flask_cors import cross_origin
 from logistic_deploy import predObj
 import numpy as np
 
@@ -63,15 +63,15 @@ def predictRoute():
 				# Prepare the input data in the format required by the model
 				input_data = [limit_bal, male, female, grad_school, university, high_school, edu_others, married, single, marriage_others, age, pay_1, pay_2, pay_3, pay_4,pay_5, pay_6, bill_amt_1, bill_amt_2, bill_amt_3, bill_amt_4, bill_amt_5, bill_amt_6, pay_amt_1, pay_amt_2, pay_amt_3, pay_amt_4, pay_amt_5, pay_amt_6]
 		# Make the prediction using the model
-		prediction = self.predObj.predict(input_data)
-		
-		# Prepare the response to be returned to the client
-		response = jsonify({'prediction': prediction})
-		
-		# Return the response
-		return response
-	except ValueError:
-		return jsonify({'error': 'Invalid input data. Please check your input and try again.'})
+				self.prediction = self.predObj.predict(input_data)
+				
+				# Prepare the response to be returned to the client
+				response = jsonify({'prediction': prediction})
+				
+				# Return the response
+				return response
+			except ValueError:
+				return jsonify({'error': 'Invalid input data. Please check your input and try again.'})
 	except Exception as e:
 		return jsonify({'error': str(e)})
 
