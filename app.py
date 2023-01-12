7cb310e3b90a6e784aab26bd8d245e0b5692b4f5
DataScienceMLGrpM <121807393+DataScienceMLGrpM@users.noreply.github.com>
2023-01-13 00:27:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -60,26 +60,26 @@ def predictRoute():
 				pay_amt_5 = float(data['pay_amt_5'])
 				pay_amt_6 = float(data['pay_amt_6'])				
 				# Prepare the input data in the format required by the model
-                # data = request.json['input_data']                    
-                
-                
-                input_data = {'limit_bal':limit_bal,'male':male,'female':female,'grad_school':grad_school,'university':university,'high_school':high_school,'edu_others':edu_others,'married':married,'single':single,'marriage_others':marriage_others,'age':age,'pay_1':pay_1,'pay_2':pay_2,'pay_3':pay_3,'pay_4':pay_4,'pay_5':pay_5,'pay_6':pay_6,'bill_amt_1':bill_amt_1,'bill_amt_2':bill_amt_2,'bill_amt_3':bill_amt_3,'bill_amt_4':bill_amt_4,'bill_amt_5':bill_amt_5,'bill_amt_6':bill_amt_6,'pay_amt_1':pay_amt_1,'pay_amt_2':pay_amt_2,'pay_amt_3':pay_amt_3,'pay_amt_4':pay_amt_4,'pay_amt_5':pay_amt_5,'pay_amt_6':pay_amt_6}
-                print('Input data is:     ', input_data)
-                
-				# Make the prediction using the model
-				pred=predObj()
-                prediction = pred.predict_log(input_data)
-                #result = clntApp.predObj.predict_log(data)
-                print('result is        ',prediction)
-                return render_template('results.html',result=prediction)				
-				# Prepare the response to be returned to the client
+				# data = request.json['input_data']                    
+
+
+				input_data = {'limit_bal':limit_bal,'male':male,'female':female,'grad_school':grad_school,'university':university,'high_school':high_school,'edu_others':edu_others,'married':married,'single':single,'marriage_others':marriage_others,'age':age,'pay_1':pay_1,'pay_2':pay_2,'pay_3':pay_3,'pay_4':pay_4,'pay_5':pay_5,'pay_6':pay_6,'bill_amt_1':bill_amt_1,'bill_amt_2':bill_amt_2,'bill_amt_3':bill_amt_3,'bill_amt_4':bill_amt_4,'bill_amt_5':bill_amt_5,'bill_amt_6':bill_amt_6,'pay_amt_1':pay_amt_1,'pay_amt_2':pay_amt_2,'pay_amt_3':pay_amt_3,'pay_amt_4':pay_amt_4,'pay_amt_5':pay_amt_5,'pay_amt_6':pay_amt_6}
+				print('Input data is:     ', input_data)
+
+						# Make the prediction using the model
+						pred=predObj()
+				prediction = pred.predict_log(input_data)
+				#result = clntApp.predObj.predict_log(data)
+				print('result is        ',prediction)
+				return render_template('results.html',result=prediction)				
+						# Prepare the response to be returned to the client
 				#response = jsonify({'prediction': prediction})				
 				# Return the response
 				#return response
 			except ValueError:
 				return jsonify({'error': 'Invalid input data. Please check your input and try again.'})
-        else:
-            return jsonify({'error': 'Expected a JSON request'})        
+		else:
+		    return jsonify({'error': 'Expected a JSON request'})        
 	except Exception as e:
 		return jsonify({'error': str(e)})
 
